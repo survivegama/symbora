c4ab22f7e1063d6f160cf9aca8883eba616dcfe3
Maruan <you@example.com>
2019-02-20 19:39:40 -0300

    php

---
+++ conexao.php
@@ -0,0 +1,14 @@
+<?php
+
+
+
+
+
+$con = mysqli_connect("mysql995.umbler.com","maruan","maruan10","surviveposts");
+
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+?>
