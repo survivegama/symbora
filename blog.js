44d2d5d0b7a50f8493389a4dff13953101747cb8
Maruansilva <maruansilva@mssdev.com.br>
2019-02-22 09:59:01 -0300

    modal

--- blog.js
+++ blog.js
@@ -64,6 +64,7 @@ function writeDetails() {
 if(valor == '' || undefined){
     window.open("./downloads/CronogramaCarnaval.pdf", "minhaJanela");
 }
+alert(valor);
 
 switch(valor) {
     case '1':
@@ -79,6 +80,9 @@ switch(valor) {
 
       case '4':
       // code block
+      break;
+      case undefined:
+      window.open("./downloads/CronogramaCarnaval.pdf", "minhaJanela");
       break;
       
     
